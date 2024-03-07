690cb79856a7caa810091ac83cf0b70ef9fb5b4e
Isabelle Hansen-Yang <hansenyang@proton.me>
2024-03-06 20:48:46 -0600

    restart

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -50,7 +50,6 @@ public class CustomList extends ArrayAdapter<City> {
      * @return
      */
     public int getCount(){
-        return cities.size();
     }
     /**
      * this adds a city object to the list
